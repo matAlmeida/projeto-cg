23708b1016e848e22d2448ee643b9fd51cf54068
sya-kun <catdog_39@yahoo.com.br>
2017-05-16 22:06:31 -0300

    Descrição do tutorial 06

--- source/tutorial-06/main.py
+++ source/tutorial-06/main.py
@@ -1,3 +1,15 @@
+﻿"""
+No tutorial-06, faremos o mesmo que o tutorial passado. Mas, desta vez carregaremos a imagem à partir de um arquivo. Visto isto, é importante que o arquivo a ser carregado esteja em uma pasta visível pelo python, de preferência na mesma em que se encontra o arquivo "main.py".
+Em "LTexture.py" nós iremos importar a classe "Image" e o método "open" da biblioteca PIL.
+loadTextureFromFile() é a nossa nova função para carregar texturas a partir de um arquivo, que recebe uma string com o caminho do arquivo como argumento. Logo no começo da funçao, temos a nossa flag de textura carregada. Nas próximas linhas, nós carregamos nosso arquivo com a função open(), e convertemos os dados dos pixels para o padrão RGBA com o método de classe tobytes(). Agora, basta passar as informações para a função loadTextureFromPixels32(), apresentada no tutorial anterior, para gerar as texturas. Os índices 0 e 1 do vetor da imagem representam a largura e altura, respectivamente.
+Após carregar a textura, é uma boa prática fechar o arquivo com close().
+
+Na nossa biblioteca LUtil, habilitaremos o uso de texturas com glEnable() na função initGL().
+E faremos a chamada da função loadTextureFromFile() para carregar nossa imagem. Tenha certeza de que o arquivo "opengl.png" se encontre na mesma pasta que os arquivos python.
+
+Por fim, na função render(), faremos o mesmo que foi feito no tutorial-05.
+"""
+
 from LUtil import *
 
 def main():

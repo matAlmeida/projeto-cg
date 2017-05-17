620bd8078485301887083af2f0880462d8783d88
Adshow <adson.cardosocic@yahoo.com.br>
2017-05-16 21:50:18 -0300

    Descrição Tutorial 05

--- source/tutorial-05/main.py
+++ source/tutorial-05/main.py
@@ -1,4 +1,32 @@
-﻿from LUtil import *
+""" Neste tutorial vamos criar a textura de um tabuleiro de xadrez e depois vamos mapea-lo.
+	
+	Primeiro, vamos criar a nossa biblioteca LTEXTURE e dentro dela vamos definir a nossa classe que se chamará LTexture também.
+Temos que criar um construtor e também um destrutor da nossa classe . Após isso, vamos criar os nossos métodos. Primeiro deles é o
+loadTextureFromPixels32() que recebe os dados da textura a ser criada e gera uma nova textura. Temos uma função para liberar da me-
+moria desalocando os dados da textura. 
+	Dentro da nossa função render() vamos posicionar a nossa textura e mapea-lo e vamos por métodos de consulta da nossa classe
+como getTextureId(), textureWidth() e textureHeight() , para passar o identificador , largura e altura da nossa textura. Antes de
+começar a carregar os dados, temos que ter certeza que não está com lixo a textura, para depois atribuir as dimensões. Definimos
+a função glGenTextures() para gerar um identificador para a nossa textura. Com a a função glTexImage2D() vamos passar todos o argumentos
+para o nosso gerador de textura como: tipo, formato dos pixels com a textura armazenada, largura, altura, formato dos dados, largura
+da borda, e o endereço dos pixels que você está passando. Com a função glTexParameter() vamos definir de que modo a textura será exibida.
+	Na nossa função render() vamos verificar se existe uma textura a ser criada, definir a posição dela na tela e passar os dados
+para cria-la. Agora vamos chamar a função glTexCoord() para passar uma textura á um vértice, de forma parecida de como passavamos uma
+cor a um vértice nos tutoriais anteriores. As texturar não trabalham como coordenas x, y, z e sim com eixos, o horizontal e o vertical
+e esses eixos variam de 0 à 1, mesmo que a quantidade de texturas/pixels seja superior a 1.
+	Dentro de glInit() vamos adicionar a função loadMedia() para carregar os pixels da textura e glEnable() para habilitar a textu-
+rização em 2D. Vamos passar 128x128 para loadMedia() para o tamanho do nosso tabuleiro. Para colorir vamos por uma verificação, se o
+quinto bit de x e y são diferentes vamos passar branco, senão, passaremos vermelho (sempre com RGB). Por fim vamos rendezirar o tabuleiro
+no centro da tela."""
+
+
+
+
+
+
+
+
+from LUtil import *
 
 def main():
 	#Inicializando FreeGLUT

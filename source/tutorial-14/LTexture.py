298d0e86232d288a0d42f0eab21149a691ab8346
levy <levyssantiago@gmail.com>
2017-05-26 18:12:25 -0300

    Tutorial-14 adicionado - COM ERRO

---
+++ source/tutorial-14/LTexture.py
@@ -0,0 +1,170 @@
+from LFRect import *
+
+DEFAULT_TEXTURE_WRAP = GL_REPEAT
+
+class LTexture:
+	def __init__(self):
+		#Inicializando textura ID
+		self.mTextureID = 0
+		#Inicializando dimensões da textura
+		self.mTextureWidth = 0
+		self.mTextureHeight = 0
+		#Dimensões da imagem
+		self.mImageWidth = 0
+		self.mImageHeight = 0
+		#Pixels atuais
+		self.mPixels = None
+
+	@staticmethod
+	def freeTexture(self):
+		#Deletando textura
+		if(self.mTextureID != 0):
+			glDeleteTextures(1,self.mTextureID)
+			self.mTextureID = 0
+		#Deletando pixels
+		if(self.mPixels != None):
+			del self.mPixels
+			self.mPixels = None
+		self.mTextureWidth = 0
+		self.mTextureHeight = 0
+		self.mImageHeight = 0
+		self.mImageWidth = 0
+
+	def __del__(self):
+		#Limpa dados da textura se preciso
+		self.freeTexture(self)
+
+	def loadTextureFromPixels32(self,pixels,imgWidth,imgHeight,texWidth,texHeigth):
+		#Obtendo dimensoes de imagem
+		self.mImageWidth = imgWidth
+		self.mImageHeight = imgHeight
+		self.mTextureWidth = texWidth
+		self.mTextureHeight = texHeigth
+
+		#Gera textura ID
+		glGenTextures(1,self.mTextureID)
+
+		#Define a ID da textura
+		self.mTextureID = 1
+
+		#Cria textura ID
+		glBindTexture(GL_TEXTURE_2D,self.mTextureID)
+
+		#Gera textura
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,self.mTextureWidth,self.mTextureHeight,0,GL_RGBA,GL_UNSIGNED_BYTE,self.mPixels)
+
+		#Definindo parâmetros da textura
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, DEFAULT_TEXTURE_WRAP)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, DEFAULT_TEXTURE_WRAP)
+
+		#Liberando textura
+		glBindTexture(GL_TEXTURE_2D,0)
+
+		#Procurando erros
+		erro = glGetError()
+		if(erro != GL_NO_ERROR):
+			print("Erro ao carregar textura de %p pixels! %s\n",self.mPixels,gluErrorString(erro))
+			return False
+		return True
+
+	def loadTextureFromFile(self,imagem):
+		textureLoaded = False
+		#Desalocando dados da textura
+		self.freeTexture(self)
+
+		pixelsLoaded = False
+		im = Image.open(imagem).convert("RGBA")
+		if(im != None):
+			#Inicializando dimensões
+			imgWidth = im.width
+			imgHeight = im.height
+
+			#Calculando dimensões da textura
+			texWidth = self.powerOfTwo(imgWidth)
+			texHeigth = self.powerOfTwo(imgHeight)
+
+			#Obtendo dimensoes de imagem
+			self.mImageWidth = imgWidth
+			self.mImageHeight = imgHeight
+			self.mTextureWidth = texWidth
+			self.mTextureHeight = texHeigth
+
+			imagem = im.tobytes("raw","RGBA",0,-1)
+			textureLoaded = self.loadTextureFromPixels32(imagem,imgWidth,imgHeight,texWidth,texHeigth)
+
+			pixelsLoaded = True
+		im.close()
+		if(pixelsLoaded == False):
+			print("Não foi possível carregar a imagem!")
+		return pixelsLoaded
+
+	def render(self,x,y,clip = None):
+		#Se a textura existe
+		if(self.mTextureID != 0):
+			#Coordenadas de textura
+			texTop = 0.0
+			texBottom = self.mImageHeight/self.mTextureHeight
+			texLeft = 0.0
+			texRight = self.mImageWidth/self.mTextureWidth
+
+			#Coordenadas de vértice
+			quadWidth = self.mImageWidth
+			quadHeight = self.mImageHeight
+
+			if(clip != None):
+				#Coordenadas de textura
+				texLeft = clip.x / self.mTextureWidth
+				texRight = (clip.x + clip.w) / self.mTextureWidth
+				texTop = clip.y / self.mTextureHeight
+				texBottom = (clip.y + clip.h) / self.mTextureHeight
+
+				#Coordenadas de vertice
+				quadWidth = clip.w
+				quadHeight = clip.h
+
+			#Movendo para o ponto de renderização
+			glTranslatef(x,y,0)
+
+			#Definindo textura ID
+			glBindTexture(GL_TEXTURE_2D,self.mTextureID)
+
+			#Renderizando quadrados texturizados
+			glBegin(GL_QUADS)
+			glTexCoord2f(texLeft,texTop)
+			glVertex2f(0,quadHeight)
+			glTexCoord2f(texRight,texTop)
+			glVertex2f(quadWidth,quadHeight)
+			glTexCoord2f(texRight,texBottom)
+			glVertex2f(quadWidth,0)
+			glTexCoord2f(texLeft,texBottom)
+			glVertex2f(0,0)
+			glEnd()
+
+	def getPixelData32(self):
+		return self.mPixels
+
+	def getTextureID(self):
+		return self.mTextureID
+
+	def textureWidth(self):
+		return self.mTextureWidth
+
+	def imageWidth(self):
+		return self.mImageWidth
+
+	def imageHeight(self):
+		return self.mImageHeight
+
+	def textureHeight(self):
+		return self.mTextureHeight
+
+	def powerOfTwo(self,num):
+		if(num != 0):
+			num -= 1
+			num = num | (num >> 4)
+			num = num | (num >> 8)
+			num = num | (num >> 16)
+			num += 1
+		return num

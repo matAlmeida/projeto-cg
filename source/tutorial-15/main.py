ab463e589e9960f59baded9ff0bf5dbcbfc5b078
levy <levyssantiago@gmail.com>
2017-05-27 19:02:33 -0300

    Tutorial-15 adicionado funcionando

---
+++ source/tutorial-15/main.py
@@ -0,0 +1,41 @@
+from LUtil import *
+
+def main():
+	#Inicializando FreeGLUT
+	# Caso esteja dando erro nessa linha no linux é preciso instalar o freglut
+	# sudo apt-get install freeglut3-dev
+	glutInit([]);
+
+	#Criando contexto OpenGL 2.1
+	glutInitContextVersion(2, 1);
+
+	#Criando janela dupla de buffer
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(SCREEN_WIDTH,SCREEN_HEIGHT);
+	glutCreateWindow(b"OpenGL");
+
+	#Chamando a função de inicialização da biblioteca gráfica
+	if(initGL() == False):
+		print("Não foi possível iniciar as bibliotecas gráficas!\n");
+		return 1;
+
+	#Carregando função loadMedia
+	if(loadMedia() == False):
+		print("Não foi possível carregar midia!")
+		return 2
+
+	#Definindo tecla
+	glutKeyboardFunc(handlekeys)
+
+	#Setando a função de renderização
+	glutDisplayFunc(render);
+
+	#Definindo main loop
+	glutTimerFunc(1000 // SCREEN_FPS, runMainLoop,0);
+
+	#Iniciando GLUT main loop
+	glutMainLoop();
+
+	return 0;
+
+main()

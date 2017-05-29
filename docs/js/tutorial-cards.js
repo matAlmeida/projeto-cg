400f1da92c472d18976847708c0bd89d8036efb4
Matheus Almeida <mat.almeida@live.com>
2017-05-28 21:32:39 -0300

    imagens padrão e script do tutorial-cards

---
+++ docs/js/tutorial-cards.js
@@ -0,0 +1,37 @@
+function createTutoCard(tutorialNumber, tutorialTitle, tutorialLink, tutorialLibs, tutorialDescription) {
+
+    let panel = document.querySelector("#cards-tuto");
+
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.classList.add("hoverable");
+    div.classList.add("col");
+    div.classList.add("s4");
+
+    div.innerHTML = `
+    <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="images/tutorial-${tutorialNumber}.png">
+    </div>
+    <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Tutorial ${tutorialNumber}<i class="material-icons right">more_vert</i></span>
+        <p><a href="${tutorialLink}" target="_blank">${tutorialTitle}</a></p>
+    </div>
+    <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Bibliotecas utilizadas<i class="material-icons right">close</i></span>
+        <p>${tutorialLibs}</p>
+        <span class="card-title grey-text text-darken-4">Descrição</span>
+        <p>${tutorialDescription}</p>
+    </div>
+    `;
+
+    panel.appendChild(div);
+}
+
+function genTutoCards(tutos) {
+
+    tutos.forEach(function(element) {
+        createTutoCard(element["Numero"], element["Titulo"], element["Link"], element["Bibliotecas"], element["Descricao"]);
+    }, this);
+
+    // document.querySelector("#progressBar").innerHTML = "";
+}

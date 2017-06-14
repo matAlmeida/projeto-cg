0d35e6fbf57dbe0d20c4b35711894e6764655223
levy <levyssantiago@gmail.com>
2017-06-14 11:18:33 -0300

    Pacman andando!

--- source/tutorial-02/LUtil.py
+++ source/tutorial-02/LUtil.py
@@ -20,6 +20,14 @@ gColorMode = COLOR_MODE_CYAN
 
 gRenderMode = RENDER_SQUARE
 
+gAberturaBoca = 0
+bocaAbrindo = True
+
+gCameraX = 0
+gCameraY = 0
+gAngle = mt.pi
+gBocaDir = 0
+
 #Scala de projeção
 gProjectionScale = 1.0; #Tipo: GLfloat
 
@@ -32,6 +40,9 @@ def initGL():
 	#Inicializando Matriz de modelo de exibição (Modelview)
 	glMatrixMode(GL_MODELVIEW)
 	glLoadIdentity()
+
+	#Salvando a matriz padrão do modelo de exibição (modelview)
+	glPushMatrix()
 
 	#Inicializando a tela com a cor preta
 	glClearColor(0,0,0,1)
@@ -130,9 +141,22 @@ def renderCircle():
 
 
 def renderPacman(abertura):
+	global gBocaDir
 	r = 50
 	alpha = 0.0
-	dalpha = mt.pi / 20
+	dalpha = mt.pi / 40
+
+	#Movendo a câmera para uma posição
+	glTranslatef(gCameraX,gCameraY,0)
+
+	if (gBocaDir == 0):
+		glRotatef(0,0,0,1)
+	elif(gBocaDir == 2):
+		glRotatef(180,0,0,1)
+	elif(gBocaDir == 3):
+		glRotatef(90,0,0,1)
+	else:
+		glRotatef(-90,0,0,1)
 
 	x = r * mt.cos(alpha)
 	y = r * mt.sin(alpha)
@@ -142,7 +166,7 @@ def renderPacman(abertura):
 		x = r * mt.cos(alpha)
 		y = r * mt.sin(alpha)
 	glColor3f(1,1,0)
-	for i in range(40 - (abertura * 2)):
+	for i in range(80 - (abertura * 2)):
 		glVertex2f(x, y)
 		glVertex2f(0.0, 0.0)
 		alpha += dalpha
@@ -153,12 +177,18 @@ def renderPacman(abertura):
 
 
 def render():
+	global gAberturaBoca, bocaAbrindo
+
 	#Limpando o buffer de cor
 	glClear(GL_COLOR_BUFFER_BIT)
 
 	#Reiniciando a matriz Modelview
 	glMatrixMode(GL_MODELVIEW)
+	glPopMatrix()
 	glLoadIdentity()
+
+	#Salvando a matriz patrão novamente
+	glPushMatrix()
 
 	#Movendo a  para o centro da tela
 	glTranslatef(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0, 0.0)
@@ -168,7 +198,17 @@ def render():
 	elif(gRenderMode == RENDER_CIRCLE):
 		renderCircle()
 	else:
-		renderPacman(2)
+		renderPacman(gAberturaBoca)
+		if(bocaAbrindo):
+			gAberturaBoca += 1
+			if(gAberturaBoca > 10):
+				bocaAbrindo = False
+				gAberturaBoca -= 2
+		else:
+			gAberturaBoca -= 1
+			if(gAberturaBoca < 0):
+				bocaAbrindo = True
+				gAberturaBoca += 2
 
 	#Atualizando tela
 	glutSwapBuffers()
@@ -182,11 +222,9 @@ def runMainLoop(val):
 	glutTimerFunc(1000 // SCREEN_FPS, runMainLoop, val)
 
 def handleKeys(key,x,y):
-	global gColorMode
-	global gRenderMode
-	global gProjectionScale
+	global gColorMode, gRenderMode, gProjectionScale, gCameraX, gCameraY, gAngle, gBocaDir
 	key = ord(key)
-	print ("key: "+str(key))
+	#print ("key: "+str(key))
 	#Se o usuário pressiona q
 	if(key == 113):
 		#Altera modo de cor
@@ -213,9 +251,33 @@ def handleKeys(key,x,y):
 			gRenderMode = RENDER_PACMAN
 		else:
 			gRenderMode = RENDER_SQUARE
-		glutPostRedisplay()
+	#se o usuario pressiona a
+	elif(key == 97):
+		gCameraX -= 16
+		gBocaDir = 2
+	#se o usuario pressiona d
+	elif(key == 100):
+		gCameraX += 16
+		gBocaDir = 0
+	#se o usuario pressiona w
+	elif(key == 119):
+		gCameraY -= 16
+		gBocaDir = 1
+	#se o usuario pressiona s
+	elif(key == 115):
+		gCameraY += 16
+		gBocaDir = 3
 
-	#Atualizando matriz de projeção
-	glMatrixMode(GL_PROJECTION)
+	
+	glMatrixMode( GL_PROJECTION );
 	glLoadIdentity()
 	glOrtho(0.0, SCREEN_WIDTH * gProjectionScale, SCREEN_HEIGHT * gProjectionScale, 0.0, 1.0, -1.0)
+
+	#Retirando a matriz salva na pilha e redefinindo
+	glMatrixMode(GL_MODELVIEW)
+	glPopMatrix()
+	glLoadIdentity()
+
+	#Salvando a matriz padrão novamente e a translação de câmera
+	glPushMatrix()
+	

137a3e19ea5583185e80a54a3d8e328d9e78d7be
Matheus Almeida <mat.almeida@live.com>
2017-07-12 17:24:49 -0300

    remaking pacman

--- source/desafio-02/Pacman.py
+++ source/desafio-02/Pacman.py
@@ -4,65 +4,81 @@ from OpenGL.GLUT import *
 import math as mt
 
 class Pacman:
-    def __init__(self, size):
-        self._aberturaBoca = 0
-        self._bocaAbrindo = True
-        self._bocaDir = 0
-        self._size = size;
-        self._alpha = 0.0
-        self._dalpha = mt.pi / 40
+
+    def __init__(self, size, maxAmplitude, speed = 40, color = [1, 1, 0]):
+        self.__size = size
+        self.__angle = 0
+        self.__maxAmplitude = maxAmplitude
+        self.__speed = speed
+        self.__color = color
+        
+        # Starting the variables
+        self.__starting()
+
+    def __starting(self):
+        self.__isOpen = True
+        self.__actualAmplitude = self.__maxAmplitude
+        self.__alpha = 0.0
+        self.__dalpha = mt.pi / self.__speed
+        self.__renderRange = (self.__speed * 2)
+
+    def __calculateX(self):
+
+        return self.__size * mt.cos(self.__alpha)
+    
+    def __calculateY(self):
+
+        return self.__size * mt.sin(self.__alpha)
+
+    def __calculateAlpha(self):
+
+        return self.__alpha += self.__dalpha
+
+    def __ôpenTheTcheka(self):
+        if(self.__isOpen):
+            self.__actualAmplitude += 1
+            if(self.__actualAmplitude > 10):
+                self.__isOpen = False
+                self.__actualAmplitude -= 2
+        else:
+            self.__actualAmplitude -= 1
+            if(self.__actualAmplitude < 0):
+                self.__isOpen = True
+                self.__actualAmplitude += 2
+        
+        return
+
+    def changeSize(self, newSize):
+        self.__size = newSize
+
+        return
 
     def render(self):
-        #Movendo a câmera para uma posição
-        glTranslatef(gCameraX,gCameraY,0)
+
+        # glTranslatef(gCameraX, gCameraY, 0)
 
         x = self.__calculateX()
         y = self.__calculateY()
+
         glBegin(GL_TRIANGLES)
-        for i in range(self._aberturaBoca):
+
+        for i in range(self.__maxAmplitude):
             __calculateAlpha()
             x = self.__calculateX()
             y = self.__calculateY()
-        glColor3f(1,1,0)
-        for i in range(80 - (self._aberturaBoca * 2)):
+
+        glColor3f(self.__color[0], self.__color[1], self.__color[2])
+
+        for i in range(self.__renderRange - (self.__actualAmplitude * 2)):
             glVertex2f(x, y)
             glVertex2f(0.0, 0.0)
             __calculateAlpha()
             x = self.__calculateX()
             y = self.__calculateY()
             glVertex2f(x, y)
+
         glEnd()
 
-        #Verificando e alterando abertura da boca
-        if(self._bocaAbrindo):
-            self._aberturaBoca += 1
-            if(self._aberturaBoca > 10):
-                self._bocaAbrindo = False
-                self._aberturaBoca -= 2
-        else:
-            self._aberturaBoca -= 1
-            if(self._aberturaBoca < 0):
-                self._bocaAbrindo = True
-                self._aberturaBoca += 2
+        self.__ôpenTheTcheka()
 
-    def changeSize(self, newSize):
-        self._size = newSize
-
-        return True
-
-    def rotate(self, alpha):
-        for i in range(mt.fabs(alpha)):
-            self._bocaDir += i
-            glRotatef(self._bocaDir, 0, 0, 1)
-
-    def __calculateX(self):
-
-        return self._size * mt.cos(self._alpha)
-    
-    def __calculateY(self):
-
-        return self._size * mt.sin(self._alpha)
-
-    def __calculateAlpha(self):
-
-        return self._alpha += self._dalpha
+        return

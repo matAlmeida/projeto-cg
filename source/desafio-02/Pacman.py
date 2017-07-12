006597ef4de641dfcfc37fbdb66262ec579a9f64
Matheus Almeida <mat.almeida@live.com>
2017-07-12 15:47:33 -0300

    criando class pacman

---
+++ source/desafio-02/Pacman.py
@@ -0,0 +1,68 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import math as mt
+
+class Pacman:
+    def __init__(self, size):
+        self._aberturaBoca = 0
+        self._bocaAbrindo = True
+        self._bocaDir = 0
+        self._size = size;
+        self._alpha = 0.0
+        self._dalpha = mt.pi / 40
+
+    def render(self):
+        #Movendo a câmera para uma posição
+        glTranslatef(gCameraX,gCameraY,0)
+
+        x = self.__calculateX()
+        y = self.__calculateY()
+        glBegin(GL_TRIANGLES)
+        for i in range(self._aberturaBoca):
+            __calculateAlpha()
+            x = self.__calculateX()
+            y = self.__calculateY()
+        glColor3f(1,1,0)
+        for i in range(80 - (self._aberturaBoca * 2)):
+            glVertex2f(x, y)
+            glVertex2f(0.0, 0.0)
+            __calculateAlpha()
+            x = self.__calculateX()
+            y = self.__calculateY()
+            glVertex2f(x, y)
+        glEnd()
+
+        #Verificando e alterando abertura da boca
+        if(self._bocaAbrindo):
+            self._aberturaBoca += 1
+            if(self._aberturaBoca > 10):
+                self._bocaAbrindo = False
+                self._aberturaBoca -= 2
+        else:
+            self._aberturaBoca -= 1
+            if(self._aberturaBoca < 0):
+                self._bocaAbrindo = True
+                self._aberturaBoca += 2
+
+    def changeSize(self, newSize):
+        self._size = newSize
+
+        return True
+
+    def rotate(self, alpha):
+        for i in range(mt.fabs(alpha)):
+            self._bocaDir += i
+            glRotatef(self._bocaDir, 0, 0, 1)
+
+    def __calculateX(self):
+
+        return self._size * mt.cos(self._alpha)
+    
+    def __calculateY(self):
+
+        return self._size * mt.sin(self._alpha)
+
+    def __calculateAlpha(self):
+
+        return self._alpha += self._dalpha
--- source/desafio-02/main.py
+++ source/desafio-02/main.py
@@ -1,26 +1,3 @@
-""" No tutorial 02 vamos utilizar um sistema de coordenadas com as dimensões 640x480 e vamos também colori-lo. Primeiro,
-na nossa biblioteca auxiliar LUtil, vamos definir o tamanho da tela que iremos trabalhar e definir a quantidade de frames
-por segundo com as contantes SCREEN_WIDTH, SCREEN_HEIGHT E SCREEN_FPS . Vamos também iniciar as cores do desenho como a
-constante COLOR_MODE_CYAN = 0 E COLOR_MODE_MULTI = 1 . 
-    Ainda em LUtil vamos chamar a função glInit() e vamos fazer os processos idênticos do Tutorial 01 , usando as funções
-glMatrixMode() e glLoadIdenty() mas adicionaremos uma nova função, a glOrtho() que multiplica a matriz atual por uma matriz
-ortográfica (2 dimensões) usando os valores esquerdo, direito , cima , baixo, perto e longe como argumentos. A nossa função
-update() continuará sem utilidade. Na função render() vamos limpar o buffer de cor com glClear() e vamos utlizar uma matriz
-Model_View para aplicar mudanças na nossa figura. A diferença entre a Model_View e a Model_Projection é que uma controla a 
-visualização e a outra controla a renderização. Vamos usar a função glTranslate() para mudar nossa figura do canto superior
-esquerdo , para o centro da tela. Após isso, vamos definir a próxima cor do vértice com a função glColor() usando parâmetros
-do sistema RGB e vamos definir a posição do vértice com glVertex(). Observe que quando enviamos um vertice o OpenGL toma como
-cor base a ultima cor passada. Não podemos esquecer de atualizar a tela com a função glutSwapBuffers().
-    Outra novidade é a função handleKeys() que vamos usar para verificar a tecla pressionada e realizar uma ação se tal 
-condição for satisfeita. No nosso caso se 'q' for pressionado ele troca de Ciano para Colorido ou vice-versa. Também vamos usar
-a tecla 'e' para mudar a escala do nosso desenho , usando a função gProjectionScale, vamos mudar ela para o dobro do tamanho,
-depois para metade do tamanho e depois ela volta para o tamanho inicial. 
-    A função main() é exatamente igual a do Tutorial 01 , muda apenas a adição de glutKeyboardFunc() para o uso do teclado."""
-
-
-
-
-
 from LUtil import *
 
 def main():
